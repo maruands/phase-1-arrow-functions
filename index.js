95f28465b351aaf961ff2ed1f379b36a295f44ff
maruands <maruands@gmail.com>
2024-01-11 09:32:54 +0300

    functions and arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+//function called divide
+function divide()
+{
+    return 2000 / 100;
+}
+
+//arrow function
+const square = (x) => x * x;
+
+// arrow function for add
+const add = (x,y) => x + y;
